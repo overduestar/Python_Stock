a7f1251b08e94f78629508fd91e33a0f0e1ec4fe
overduestar <overduestar@gmail.com>
2016-04-06 22:38:32 +0800

    1. improve parse_stock_table_htm2csv() function perfomace, and rewrite parse_stock_table_htm2csv() code flow 2. revise global variable naming rule

--- parse.py
+++ parse.py
@@ -9,12 +9,46 @@ from datetime import datetime, timedelta
 import time
 import glob
 from os import path
+import codecs
+
+class StockTableInfo:
+        __item_list = []
+        __item_num = 0
+        def __init__(self):
+                self.item_list = []
+                self.item_num = 0
+        def append(self, serial_number, name):
+                item = []
+                item.append(serial_number)
+                item.append(name)
+                self.__item_list.append(item)
+                self.__item_num+=1
+        def remove(self):
+                item = []
+                if (self.__item_num > 0):
+                        item = self.__item_list[0]
+                        self.__item_list.remove(0)
+                        self.__item_num-=1
+                return item
+        def getItem(self, idx):
+                item = []
+                if (idx < self.__item_num):
+                        item = self.__item_list[idx]
+                return item
+        def getSize(self):
+                return self.__item_num
+        def printMsg(self):
+                item = []
+                for idx in range(0, self.__item_num):
+                        item = self.__item_list[idx]
+                        print("serial number:", item[0], " ;name:", item[1])
 
 
-StockURL = "http://www.emega.com.tw/js/StockTable.xls" # For Stock context
-Stock_DAY_URL = "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report%04d%02d/%04d%02d_F3_1_8_%s.php&type=csv" # For Stock Day Info.
+g_StockURL =  "http://www.emega.com.tw/js/StockTable.htm" #"http://www.emega.com.tw/js/StockTable.xls" # For Stock context
+g_Stock_DAY_URL = "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report%04d%02d/%04d%02d_F3_1_8_%s.php&type=csv" # For Stock Day Info.
+g_StockTableDirName = 'StockTable'
 
-StockTable = 'StockTable'
+g_StockTab = StockTableInfo()
 
 def stock_download_data(year,month,stock,Stock_DAY_URL):
 
@@ -53,72 +87,74 @@ def stock_download_all_day(StartYear,StartMonth,Stock):
 		if StartYear == datetime.today().year:
 			for m in range(StartMonth,datetime.today().month+1):
 				#print('Y%04d-%02d' %(y,m))
-				#print(Stock_DAY_URL %(y,m,y,m,Stock))
-				stock_download_data(y,m,Stock,Stock_DAY_URL)
+				#print(g_Stock_DAY_URL %(y,m,y,m,Stock))
+				stock_download_data(y,m,Stock,g_Stock_DAY_URL)
 		elif y == datetime.today().year:
 			for m in range(1,datetime.today().month+1):
 				#print('R%04d-%02d' %(y,m))
-				#print(Stock_DAY_URL %(y,m,y,m,Stock))
-				stock_download_data(y,m,Stock,Stock_DAY_URL)
+				#print(g_Stock_DAY_URL %(y,m,y,m,Stock))
+				stock_download_data(y,m,Stock,g_Stock_DAY_URL)
 		elif y == StartYear:
 			for m in range(StartMonth,12+1):
 				#print('V%04d-%02d' %(y,m))
-				#print(Stock_DAY_URL %(y,m,y,m,Stock))
-				stock_download_data(y,m,Stock,Stock_DAY_URL)
+				#print(g_Stock_DAY_URL %(y,m,y,m,Stock))
+				stock_download_data(y,m,Stock,g_Stock_DAY_URL)
 		else:
 			for m in range(1,12+1):
 				#print('N%04d-%02d' %(y,m))
-				stock_download_data(y,m,Stock,Stock_DAY_URL)
+				stock_download_data(y,m,Stock,g_Stock_DAY_URL)
 		m = 0
 	stock_combin_all_day(Stock)
 
-def stock_download(StartYear,StartMonth,StockTable):
-	f = open('./stock_data/'+StockTable+'.csv','r')
-	for row in csv.DictReader(f):
-		print(row['Number'])
-		stock_download_all_day(StartYear,StartMonth,row['Number'])
+def stock_download(StartYear,StartMonth):
+        if(os.path.exists('stock_data') == False):
+                os.system('mkdir stock_data')
+        for i in range(0, g_StockTab.getSize()):
+                item = g_StockTab.getItem(i)
+                print(item[0])
+                stock_download_all_day(StartYear,StartMonth,item[0])
 
-def parse_stock_table_htm2csv(StockURL,StockTable):
-	next_line = 0
-	localList = "./stock_data/StockTable.htm"
+def getinfo_stocktable_from_browser():
+        browser_urlfile = urllib.request.urlopen(g_StockURL)
+        browser_urldata = browser_urlfile.read()
+        browser_urlinfo = codecs.decode(browser_urldata, 'big5', errors='ignore')
+        return browser_urlinfo
 
-	if(os.path.exists('stock_data') == False):
-		os.system('mkdir stock_data')
-
-	urllib.request.urlretrieve(StockURL, localList)
-
-	infile = open('./stock_data/'+StockTable+'.htm','r')
-	outfile = open('./stock_data/'+StockTable+'.csv','w')
-
-	outfile.write("Number,Name\n")
-	for line in infile.readlines():
-		if line.find('&nbsp') != -1:
-			if( line.split('&nbsp')[0].split('>')[1].split('<')[0] != ''):
-				outfile.write(line.split('&nbsp')[0].split('>')[1].split('<')[0])
-				if next_line == 0:
-					outfile.write(",")
-					next_line = 1
-				else:
-					outfile.write("\n")
-					next_line = 0		
-	infile.close()
-	outfile.close()
-
-
+def parseinfo_stocktable(browser_urlinfo):
+        if (len(browser_urlinfo) == 0):
+                return
+        urlinfo_split = re.split("<td", browser_urlinfo)
+        item_flag = 0
+        serial_num = ""
+        name = ""
+        for i in range(0, len(urlinfo_split)):
+                data = ""
+                if (urlinfo_split[i].find("td") >= 0):
+                        data = urlinfo_split[i].split('>')[1].replace("&nbsp;", "").split('<')[0]
+                if (len(data) > 0):
+                        if (item_flag == 0):
+                                serial_num = data
+                                item_flag = 1
+                        elif (item_flag == 1):
+                                name = data
+                                g_StockTab.append(serial_num, name)
+                                item_flag = 0
+        
 print('=====Start====')
 print(datetime.today())
 print(datetime.today().year)
 print(datetime.today().month)
 print(datetime.today().day)
 print('%02d' %(datetime.today().day))
-parse_stock_table_htm2csv(StockURL,StockTable)
+browser_urlinfo = getinfo_stocktable_from_browser()
+parseinfo_stocktable(browser_urlinfo)
 #stock_download_all_day(2015,2,'4545')
 #stock_download_all_day(2015,9,'3322')
 #stock_combin_all_day('4545')
 #stock_combin_all_day('3322')
-stock_download(2016,3,StockTable)
+stock_download(2016,3)
 os.system('del -rf data')
-#download_stock_all_data(StockTable,Stock_DAY_URL)
+#download_stock_all_data(g_StockTableDirName,g_Stock_DAY_URL)
 print('======End=====')
 
 
